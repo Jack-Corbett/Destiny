320f086cd38f082f46ca74383093ea2a217f2cbe
Jack-Corbett <jackcorbett16@gmail.com>
2020-05-05 22:21:01 +0100

    Updated messages

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -24,8 +24,8 @@ const TellHandler = {
     response += FORTUNE[Math.floor(Math.random()*20)] + '. ' + messages.STOP;
     
     return handlerInput.responseBuilder
-      .speak(response)
-      .withSimpleCard(skillName, response)
+      .speak(messages.SPACE)
+      .withSimpleCard(skillName, "This is your destiny!")
       .getResponse();
   }
 };
@@ -119,37 +119,38 @@ const ErrorHandler = {
 const skillName = 'Destiny';
 
 const FORTUNE = [
-  'run into an old friend',
-  'start a new adventure',
-  'stand by your convictions, don\'t be pushed around',
-  'meet someone new, who will make you smile',
-  'finish that job you\'ve been putting off',
-  'give a freind the advice they need',
-  'help someone in need',
-  'see your efforts come to fruition',
-  'get the support you need',
-  'go with the flow, as great things are coming',
-  'find the determination you need',
-  'stop underestimating yourself',
-  'get the second chance you\'ve been waiting for',
-  'find the confidence to make it happen',
-  'have a beautiful day',
-  'prove yourself',
-  'try something new',
-  'catch the eye of someone new',
-  'explore new opportunities',
-  'save someone\'s day with your kindness'
+  'run into an old friend and reminisce about old times. It will help to focus your mind on what comes next',
+  'start a new adventure that will take some unexpected twists and turns but will present an opportunity to grow',
+  'stand by your convictions, don\'t be pushed around, stay strong and you will achieve great things',
+  'meet someone new, take the time to get to know them, they will make you smile',
+  'finish that job you\'ve been putting off, it\'s the only way to clear your mind',
+  'give a freind the advice they need. If you\'re there for them, they will be there for you in the future',
+  'help someone in need, sometimes the best reward is what we can do for others',
+  'see your efforts come to fruition. Life is a marathon not a sprint and you\'ve earned this',
+  'get the support you need. It\'s okay to ask for help. Everyone needs guidance to find their way',
+  'go with the flow, sometimes it\'s difficult to break your routine, but great things are coming',
+  'find the determination you need, there\'s work to be done but you will find the strength to do it',
+  'stop underestimating yourself, believe others when they tell you what a fabulous job you do',
+  'get the second chance you\'ve been waiting for, make sure you make the most of the opportunity',
+  'find the confidence to make it happen, just go for it, you have nohing to lose',
+  'have a beautiful day, filled with opportunity, so get out there and make the most of it',
+  'prove yourself and reap the rewards, it\'s been a long time coming',                 
+  'try something new, it might be out of your comfort zone to begin with but trying new things is the only way to grow',
+  'catch the eye of someone new, this might start a new romance or just form a new friendship',
+  'explore new opportunities, you won\'t know what you\'re missing out on until you try it',
+  'save someone\'s day with your kindness. Be there for them and they will return the favour when you\'re in need of support'
 ];
 
 const messages = {
-  WELCOME: 'Welcome to Destiny, your personal fortune-teller!',
-  HELP: 'You can say tell me my fortune, or you can say exit.',
+  WELCOME: 'Welcome to Destiny, your personal fortune-teller! You can ask me to tell you your basic fortune or you can tell me your star sign to help me look deeper into your future.',
+  HELP: 'You can say tell me my fortune or I\'m, followed by your star sign to find out a deeper fortune, or you can say stop.',
   HELP_REPROMPT: 'Ask me for your fortune.',
-  SPACE: '�. ',
+  // Minimise the message period to ensure the Skill is listening for as long as possible
+  SPACE: '<break time="1ms"/>',
   FALLBACK: 'I\'m sorry I didn\'t catch that. Could you repeat your last request?',
   FALLBACK_REPROMPT: 'Could you repeat your last request?',
   ERROR: 'Sorry, there was an error.',
-  STOP: 'Have a great day! Goodbye.',
+  STOP: 'Thank you for using Desntiy! Good fortune awaits you! Goodbye.',
 }
 
 const skillBuilder = Alexa.SkillBuilders.custom();
